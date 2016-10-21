168bbc37c1555a567c792197762467e67adf6330
Alan Jaffe <alanpjaffe@gmail.com>
2016-10-20 22:28:15 -0400

    Test email

---
+++ print.py
@@ -0,0 +1,8 @@
+from subprocess import Popen, PIPE
+
+def send_email(text, subj, to, frm):
+  proc = Popen(["mailx","-s",subj,to, "-r", frm], stdin=PIPE)
+  proc.stdin.write(text)
+  proc.stdin.close()
+
+send_email("hi","test","","")
